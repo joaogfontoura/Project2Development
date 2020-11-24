fe063eb8e457a03f487a46f3fb0e4ac249ed9266
Gui <hgordo_0@hotmail.com>
2020-11-24 15:20:32 +1100

    primeiro commit

--- app/src/main/java/com/example/project2development/AttractionMenu.java
+++ app/src/main/java/com/example/project2development/AttractionMenu.java
@@ -46,6 +46,7 @@ public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallb
 
     }
     // comment para test
+    // test 2
     private void fetchLastLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
