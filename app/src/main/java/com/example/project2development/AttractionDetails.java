a6ae7e88df3646ac08ec63bcf91c48f8e4fee4fb
joaogfontoura <jggfontoura@hotmail.com>
2020-11-30 21:58:57 +1100

    - Map Ready - AttractionDetails Ready

---
+++ app/src/main/java/com/example/project2development/AttractionDetails.java
@@ -0,0 +1,70 @@
+package com.example.project2development;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class AttractionDetails extends AppCompatActivity {
+
+    ImageView AttImg;
+    TextView AttTitle;
+    TextView AttDescription;
+
+
+    public static String title = "";
+    public static String description = "";
+    public static String image = "";
+
+
+    // usado pra fazer log.d e debugar as variaveis
+    private static final String TAG = "AttractionDetails";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attraction_details);
+
+        AttImg = findViewById(R.id.attImg);
+        AttTitle = findViewById(R.id.txtTitle);
+        AttDescription = findViewById(R.id.txtDescription);
+
+
+        String id = getIntent().getStringExtra("Id");
+
+        getDataFromFirebase(id);
+
+
+    }
+
+    private void getDataFromFirebase(String id) {
+
+        DocumentReference mDocRef = db.collection("attractions").document(id);
+
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                AttTitle.setText(documentSnapshot.getString("Name"));
+                AttDescription.setText(documentSnapshot.getString("Description"));
+
+                Glide.with(getApplicationContext()).load(documentSnapshot.getString("Image")).into(AttImg);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/project2development/AttractionMenu.java
+++ app/src/main/java/com/example/project2development/AttractionMenu.java
@@ -5,33 +5,56 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallback {
 
+    // usado pra fazer log.d e debugar as variaveis
+    private static final String TAG = "AttractionMenu";
+
+
     FusedLocationProviderClient fusedLocationProviderClient;
     Button BtnRefresh;
+    private Marker mMarker;
     Location currentLocation;
     private static final int REQUEST_CODE = 101;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private CollectionReference mColRef = db.collection("attractions");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +68,8 @@ public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallb
         fetchLastLocation();
 
     }
-    // comment para test
+
+    // function used to fetch the location of the user
     private void fetchLastLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
@@ -57,7 +81,6 @@ public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallb
             public void onSuccess(Location location) {
                 if(location != null){
                     currentLocation = location;
-                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()+""+currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
                     SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
                     supportMapFragment.getMapAsync(AttractionMenu.this);
 
@@ -66,13 +89,46 @@ public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallb
         });
     }
 
+    
     public void onMapReady(GoogleMap googleMap){
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are Here");
+        //LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        LatLng latLng = new LatLng(-33.87365, 151.20689);
+
+        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getApplicationContext(), R.raw.map_style));
+
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are Here").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         // v = zoom ( bigger value = closer in zoom )
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,12));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,14));
         googleMap.addMarker(markerOptions);
+
+        populateMarkers(googleMap);
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if(!"You are Here".equals(marker.getTitle())){
+
+
+                    String attId = marker.getSnippet();
+
+
+
+
+                    Intent markerDetails = new Intent(getApplicationContext(), AttractionDetails.class);
+
+                    markerDetails.putExtra("Id",attId);
+
+                    startActivity(markerDetails);
+
+
+                }
+
+                return false;
+            }
+        });
+
     }
 
     @Override
@@ -85,6 +141,33 @@ public class AttractionMenu extends AppCompatActivity implements OnMapReadyCallb
                 break;
         }
     }
+
+    public void populateMarkers(final GoogleMap googleMap){
+
+        mColRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+
+                    GeoPoint coordinates = documentSnapshot.getGeoPoint("Coordinates");
+                    double coordinatesLat = coordinates.getLatitude();
+                    double coordinatesLng = coordinates.getLongitude();
+
+                    final LatLng finalcoordinates = new LatLng(coordinatesLat, coordinatesLng);
+
+                    Marker mMarker = googleMap.addMarker(
+                            new MarkerOptions()
+                                    .position(finalcoordinates)
+                                    .snippet(documentSnapshot.getId())
+                    );
+
+                }
+            }
+        });
+
+    }
+
+
 
     public void returnMainPage(View view) {
         startActivity(new Intent(getApplicationContext(), MainPage.class));
